f5d0f47f551722c4e1f1c9c24bef86dddc634699
Grzegorz Kowal <grzegorz@amuncode.org>
2017-09-05 07:53:42 -0300

    Convert leading tabs to spaces.
    Signed-off-by: Grzegorz Kowal <grzegorz@amuncode.org>

--- H5Zzstd.c
+++ H5Zzstd.c
@@ -21,7 +21,7 @@ const H5Z_class_t H5Z_ZSTD[1] = {{
 }};
 
 H5PL_type_t H5PLget_plugin_type(void) { return H5PL_TYPE_FILTER; }
-const void* H5PLget_plugin_info(void) { return H5Z_ZSTD; }
+const void *H5PLget_plugin_info(void) { return H5Z_ZSTD; }
 
 static size_t H5Z_filter_zstd(unsigned int flags, size_t cd_nelmts,
                               const unsigned int cd_values[], size_t nbytes,
@@ -31,46 +31,43 @@ static size_t H5Z_filter_zstd(unsigned int flags, size_t cd_nelmts,
     void *inbuf = NULL;     /* Pointer to input buffer */
     inbuf = *buf;
 
-	size_t ret_value;
-	size_t origSize = nbytes;     /* Number of bytes for output (compressed) buffer */
+    size_t ret_value;
+    size_t origSize = nbytes;     /* Number of bytes for output (compressed) buffer */
 
-	if (flags & H5Z_FLAG_REVERSE)
-	{
-		size_t decompSize = ZSTD_getDecompressedSize(*buf, origSize);
-		if (NULL == (outbuf = malloc(decompSize)))
-			goto error;
+    if (flags & H5Z_FLAG_REVERSE) {
+        size_t decompSize = ZSTD_getDecompressedSize(*buf, origSize);
+        if (NULL == (outbuf = malloc(decompSize)))
+            goto error;
 
-		decompSize = ZSTD_decompress(outbuf, decompSize, inbuf, origSize);
+        decompSize = ZSTD_decompress(outbuf, decompSize, inbuf, origSize);
 
-		free(*buf);
-		*buf = outbuf;
-		outbuf = NULL;
-		ret_value = (size_t)decompSize;
-	}
-	else
-	{
-		int aggression = 0;
-		if (cd_nelmts > 0)
-			aggression = (int)cd_values[0];
-		if (aggression > 22)
-			aggression = 22;
+        free(*buf);
+        *buf = outbuf;
+        outbuf = NULL;
+        ret_value = (size_t)decompSize;
+    } else {
+        int aggression = 0;
+        if (cd_nelmts > 0)
+            aggression = (int)cd_values[0];
+        if (aggression > 22)
+            aggression = 22;
 
-		size_t compSize = ZSTD_compressBound(origSize);
-		if (NULL == (outbuf = malloc(compSize)))
-			goto error;
+        size_t compSize = ZSTD_compressBound(origSize);
+        if (NULL == (outbuf = malloc(compSize)))
+            goto error;
 
-		compSize = ZSTD_compress(outbuf, compSize, inbuf, origSize, aggression);
+        compSize = ZSTD_compress(outbuf, compSize, inbuf, origSize, aggression);
 
-		free(*buf);
-		*buf = outbuf;
-		*buf_size = compSize;
-		outbuf = NULL;
-		ret_value = compSize;
-	}
-	if (outbuf != NULL)
-		free(outbuf);
-	return ret_value;
+        free(*buf);
+        *buf = outbuf;
+        *buf_size = compSize;
+        outbuf = NULL;
+        ret_value = compSize;
+    }
+    if (outbuf != NULL)
+        free(outbuf);
+    return ret_value;
 
 error:
-	return 0;
+    return 0;
 }
